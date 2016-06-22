b5118ece841afbb3fecb185af81fd30a7b72b5ca
Loui V <loui@betabrand.com>
2016-06-22 16:30:25 -0700

    Inital commit

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-app.set('port', (process.env.PORT || 5000));
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
 
-app.use(express.static(__dirname + '/public'));
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'public/index.html');
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 
+// app.set('port', (process.env.PORT || 5000));
 
+// app.use(express.static(__dirname + '/public'));
+
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
+
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
